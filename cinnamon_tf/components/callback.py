5235c2dff5880d3e5ed310d8df17b1914429ade4
Federico Ruggeri <federugg26@gmail.com>
2023-06-24 00:08:37 +0200

    fixed early stopping

--- cinnamon_tf/components/callback.py
+++ cinnamon_tf/components/callback.py
@@ -61,17 +61,20 @@ class TFEarlyStopping(Callback):
             self.min_delta *= 1
             self.best_value = -np.inf
 
-    def on_train_begin(
-            self,
-            logs: Optional[Dict] = None
-    ):
-        # Allow instances to be re-used
+    def reset(self):
         self.wait = 0
         self.stopped_epoch = 0
         if self.baseline is not None:
             self.best_value = self.baseline
         else:
             self.best_value = np.Inf if self.monitor_op == np.less else -np.Inf
+
+    def on_train_begin(
+            self,
+            logs: Optional[Dict] = None
+    ):
+        # Allow instances to be re-used
+        self.reset()
 
     def on_epoch_end(
             self,
@@ -101,6 +104,8 @@ class TFEarlyStopping(Callback):
         if self.stopped_epoch > 0:
             logging_utility.logger.info(f'Epoch {self.stopped_epoch + 1:.2f} early stopping')
         self.component.model.stop_training = False
+
+        self.reset()
 
     def get_monitor_value(
             self,
